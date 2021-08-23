ac561050171b813293d3b73bb380d5228fb88b78
Jesus Alberto <jesyrom0825@gmail.com>
2021-08-22 19:16:12 -0500

    algo de estilo cambio de letra titulo comando

--- src/components/LuzJardin.js
+++ src/components/LuzJardin.js
@@ -78,7 +78,7 @@ const LuzJardin = () => {
             <div className='containerCommand'>
                 
                 {/* <h3 className='titleCommand'>{conectado ? 'Conectado al servidor EMQX': 'Desconectados del servidor EMQX'}</h3> */}
-                <h3>Luz Jardin</h3>
+                <h3 className='titleCommand'>Luz Jardin</h3>
                 <div className="status">
                 <div className={ledState ? 'greenLED': 'redLED' }>
 
